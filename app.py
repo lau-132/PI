aa102c109838bc9c22833ebcd844663f1e708ba4
Lautaro Lovecchio <lautarolovecchio@gmail.com>
2022-04-06 12:37:28 +0200

    Second window(grid)

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ root = Tk()
 root.title('First steps in')
 
 #Creando un Widget "Label"
-myLabel = Label(root, text='Hello World!')
+myLabel1 = Label(root, text='Hello World!')
+myLabel2 = Label(root, text='Me llamo Lautaro Lovecchio')
 
-myLabel.pack()
+#'Shoving it' a la ventana 
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=1,column=1)
+
 
 root.mainloop()
