60672c8deddab80d7b93b0d1944138f0e331020a
Lautaro Lovecchio <lautarolovecchio@gmail.com>
2022-04-07 11:20:52 +0200

    Funciones arregladas. No entiendo muy bien cuando ha de usarse el self en los metodos

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import this
 import tkinter as tk
 from tkinter import Button, ttk
 import pygame
@@ -7,22 +8,21 @@ from pytube import YouTube
 AUDIO_PATH = '/home/usuario/Python/PI/audio'
 
 #Funcion que, dada una URL de Youtube, descarga el archivo en el directorio adecuado
-def download(self, URL):
+def download(URL):
     yt = YouTube(URL)
     checkIfAudioDirectoryExist()
     yt.streams.first().download(output_path=AUDIO_PATH)
 
 #Funcion que revisa si el directorio de .mp3's existe, sino, lo crea
-def checkIfAudioDirectoryExist(self):
+def checkIfAudioDirectoryExist():
     if os.path.exists(AUDIO_PATH):
         pass
     else:
         os.mkdir(AUDIO_PATH)
 
 #Funcion cuando el botón es pulsado para descargar
-def sendBtnClick(self, URL):
-    download(URL)
-
+def sendBtnClick():
+    download(yt_url.get())
 
 #Inicializaciones necesarias
 root = tk.Tk()
@@ -37,7 +37,7 @@ playlist_box.pack(pady=20)
 
 #Entrada de la URL de Youtube
 yt_url = ttk.Entry()
-send_btn = Button(root, text="Descargar", command=sendBtnClick(yt_url.get()))
+send_btn = Button(root, text="Descargar", command=sendBtnClick)
 
 yt_url.pack()
 send_btn.pack()
